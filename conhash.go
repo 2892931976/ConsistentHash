abaf7943dd7f4cd4d03edf571790b795402720a0
royluo <royluo06@gmail.com>
2016-02-23 23:43:08 -0800

    coding

--- conhash.go
+++ conhash.go
@@ -8,28 +8,28 @@ import (
     "sort"
 )
 
-
 type HashFunc func(date []byte) uint32
-
 type HashRing []uint32
 
 func(r HashRing) Len() int { return len(r) }
 func(r HashRing) Less(i, j int) bool { return r[i] < r[j] }
 func(r HashRing) Swap(i, j int) { r[i], r[j] = r[j], r[i] }
 
-
 type ConHash struct {
     hashfunc  HashFunc
     hashring  HashRing
+    vnodes    int
     node_map_num      map[string] int  //physical node---> node num
     vnode_map_node    map[uint32] string  //vnode key ---->physical node
     
 }
 
+
 func ConHashNew() *ConHash {
     return &ConHash {
         hashfunc: crc32.ChecksumIEEE,
         hashring: HashRing{}, 
+        vnodes: 0,
         node_map_num: make(map[string] int),
         vnode_map_node: make(map[uint32] string),
     }
@@ -53,16 +53,9 @@ func (ch *ConHash) NodeAdd(n_name string, vn_num int) {
         key = ch.NodeHash([]byte(name))
         ch.vnode_map_node[key] = n_name
         ch.hashring = append(ch.hashring, key)
+        ch.vnodes++
     }
     sort.Sort(ch.hashring)
-    for i,v := range ch.hashring {
-        fmt.Println(i, v)
-    }
-    if issort := sort.IsSorted(ch.hashring); issort {
-        fmt.Println("is sorted")
-    } else {
-        fmt.Println("not sorted")
-    }
 }
 
 func (ch *ConHash) NodeRemove(n_name string) {
@@ -83,28 +76,58 @@ func (ch *ConHash) NodeRemove(n_name string) {
             }
         }
         delete(ch.vnode_map_node, key)
+        ch.vnodes--
     }
     delete(ch.node_map_num, n_name)
-
 }
 
 
-func (ch *ConHash) NodeLookup(object string) {
+func (ch *ConHash) NodeLookup(object string) string {
     var key uint32
+    var hitindex int
 
     key = ch.NodeHash([]byte(object))
-    fmt.Println(key)
     index := sort.Search(len(ch.hashring), func(i int) bool { return ch.hashring[i] >= key })
-    fmt.Println(index)
+    if index == len(ch.hashring) {
+        hitindex = 0
+    } else {
+        hitindex = index
+    }
+    hitkey := ch.hashring[hitindex]
+    node, _ := ch.vnode_map_node[hitkey]
+    return node
+}
 
+func (ch *ConHash) NodeGetVnodes() int {
+    return ch.vnodes
 }
 
 
 func main() {
+    var name string
+    var nodestring string
     conhash := ConHashNew()
-    conhash.NodeAdd("beijing", 10)
-    conhash.NodeLookup("shanghai")
-    //conhash.NodeAdd("shanghai", 20)
-    //conhash.NodeRemove("beijing")
-
+    fmt.Println("Add Node....")
+    conhash.NodeAdd("titanic", 32)
+    conhash.NodeAdd("terminator2018", 24)
+    conhash.NodeAdd("Xenomorph", 25)
+    conhash.NodeAdd("True Lies", 10)
+    conhash.NodeAdd("avantar", 48)
+    vnodes := conhash.NodeGetVnodes()
+    fmt.Println("virtual nodes number: ",vnodes)
+    fmt.Println("Lookup Node...") 
+    for i := 0; i < 20; i++ {
+        name = "shanghai_" + strconv.Itoa(i)
+        nodestring = conhash.NodeLookup(name)
+        fmt.Println(name, "match", nodestring)
+    }
+    fmt.Println("Delete Node...")
+    conhash.NodeRemove("terminator2018")
+    vnodes = conhash.NodeGetVnodes()
+    fmt.Println("virtual nodes number: ",vnodes)
+    for i := 0; i < 20; i++ {
+        name = "shanghai_" + strconv.Itoa(i)
+        nodestring = conhash.NodeLookup(name)
+        fmt.Println(name, "match", nodestring)
+    }
 }
